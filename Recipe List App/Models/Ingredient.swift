db959dd967aff159f41ecc8cc9ed7b8088e2b48d
Lucas Van Tilburg <lucasvantilburg101202@gmail.com>
2021-06-23 22:22:35 +1000

    made seperate Ingredient class

---
+++ Recipe List App/Models/Ingredient.swift
@@ -0,0 +1,17 @@
+//
+//  Ingredient.swift
+//  Recipe List App
+//
+//  Created by Lucas Van Tilburg on 23/6/21.
+//
+
+import Foundation
+
+class Ingredient: Identifiable, Decodable {
+    
+    var id:UUID?
+    var name:String
+    var num:Int?
+    var denom:Int?
+    var unit:String?
+}
--- Recipe List App/Models/Recipe.swift
+++ Recipe List App/Models/Recipe.swift
@@ -24,11 +24,4 @@ class Recipe: Identifiable, Decodable {
 }
 
 
-class Ingredient: Identifiable, Decodable {
-    
-    var id:UUID?
-    var name:String
-    var num:Int?
-    var denom:Int?
-    var unit:String?
-}
+
